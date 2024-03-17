6e137897b4746c126b2696fb2e514314a5ba6cf8
Nouman Awais <124114395+nomicodes@users.noreply.github.com>
2024-03-17 07:42:06 +0400

    Add files via upload

---
+++ day10.js
@@ -0,0 +1,8 @@
+function showPrompt() {
+    var name = prompt('What is your name?');
+    var age = prompt('How old are you?');
+    if (name, age !== null) {
+        console.log('Hey ' + name +
+         ' how are you? and how do you feel being ' + age + ' years old?');
+    }
+}
